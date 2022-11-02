9513f9f53494ecb5d03ecde1696681f4bafcbbcf
Arash Arbabi <arbabiarash0@gmail.com>
2022-11-02 22:01:15 +0330

    feat: add a `validate` step to the training loop
    This is to see how the model evolves during the training

--- fs_mol/subgraph_ssl.py
+++ fs_mol/subgraph_ssl.py
@@ -3,6 +3,17 @@ import sys
 from pyprojroot import here as project_root
 
 sys.path.insert(0, str(project_root()))
+
+from fs_mol.models.protonet import PyG_PrototypicalNetwork
+from fs_mol.data.protonet import PyG_ProtonetBatch, get_protonet_batcher, task_sample_to_pn_task_sample
+from fs_mol.data.pyg_task_reader import pyg_task_reader_fn
+from fs_mol.utils.metrics import BinaryEvalMetrics
+from fs_mol.utils.torch_utils import torchify
+from fs_mol.utils.protonet_utils import PrototypicalNetworkTrainerConfig, evaluate_protonet_model, run_on_batches
+from fs_mol.utils.test_utils import eval_model, FSMolTaskSampleEvalResults
+from fs_mol.data import FSMolDataset, FSMolTaskSample, DataFold
+from dpu_utils.utils.richpath import RichPath
+
 import torch
 from torch.optim import Adam
 from torch_geometric.loader import DataLoader
@@ -12,6 +23,7 @@ from fs_mol.augmentation_transforms import SubGraphAugmentation
 from fs_mol.data.self_supervised_learning import FSMolSelfSupervisedInMemory
 from fs_mol.modules.pyg_gnn import PyG_GraphFeatureExtractor
 from fs_mol.modules.graph_feature_extractor import GraphFeatureExtractorConfig
+from torch_geometric.loader import DataLoader
 
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -41,8 +53,51 @@ def calculate_contrastive_loss(x1, x2):
     loss = - torch.log(loss).mean()
     return loss
 
-for epoch in range(number_of_epochs):
+def validate_model(encoderModel):
+    model = PyG_PrototypicalNetwork(encoderModel)
+    dataset = FSMolDataset.from_directory('./datasets/fs-mol', num_workers=0)
     
+    def test_model_fn(task_sample: FSMolTaskSample, temp_out_folder: str, seed: int) -> BinaryEvalMetrics:
+        batch_size = 320 # default value on `evaluate_protonet_model` in `protonet_utils.py`
+        
+        # Batch size is 320 and all of the support sets should fit into a single batch
+        train_batch = list(DataLoader(task_sample.train_samples, batch_size=batch_size))
+        
+        if len(train_batch) > 1:
+            raise ValueError('Batch Size for support set is too small')
+        
+        support_features = train_batch[0]
+        
+        test_batches = list(DataLoader(task_sample.test_samples, batch_size=batch_size - len(train_batch)))
+        
+        batches = list(map(lambda batch: PyG_ProtonetBatch(support_graphs=support_features, query_graphs=batch), test_batches))
+        
+        batch_labels = list(map(lambda batch: batch.query_graphs.y, batches))
+        
+
+        _, result_metrics = run_on_batches(
+            model,
+            batches=batches,
+            batch_labels=batch_labels,
+            train=False,
+        )
+
+        return result_metrics
+    
+    return eval_model(
+            test_model_fn=test_model_fn,
+            dataset=dataset,
+            train_set_sample_sizes=[16, 128], # This is from parse_command_line in `protonet_train.py`
+            out_dir=None, # What is save Dir? Seems like this is None
+            num_samples=5, # This is from `--validation-num-samples` parse_command_line in `protonet_train.py` 
+            test_size_or_ratio=512, # `--validation-query-set-size` parse_command_line in `protonet_train.py` 
+            task_reader_fn=pyg_task_reader_fn,
+            fold=DataFold.VALIDATION, # This is used from `validate_by_finetuning_on_tasks` in `protonet_utils.py` This is validation because during the training loop inside the `train_loop` function FS-MOL calls `validate_by_finetuning_on_tasks`
+            seed=0 # This is from `validate_by_finetuning_on_tasks`'s default parameters
+        )
+    
+for epoch in range(1, number_of_epochs + 1):
+    # Pre_training:
     for batch_1, batch_2 in tqdm(zip(dl, dl2), total=len(dl)):
         optm.zero_grad()
         features_1 = model(batch_1)
@@ -50,3 +105,7 @@ for epoch in range(number_of_epochs):
         loss = calculate_contrastive_loss(features_1, features_2)
         loss.backward()
         optm.step()
+    
+    # Validation:
+    if epoch % 100 == 0:
+        validate_model(encoderModel=model)
