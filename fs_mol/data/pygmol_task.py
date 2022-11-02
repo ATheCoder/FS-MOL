7d5c06b349b204f82c72fd8f77862d201acd94f2
Arash Arbabi <arbabiarash0@gmail.com>
2022-11-02 21:56:14 +0330

    feat: add `PyGMolTask` data structure
    This data structure can be used by FSMol's `StratifiedTaskSampler`

---
+++ fs_mol/data/pygmol_task.py
@@ -0,0 +1,23 @@
+from more_itertools import partition
+from fs_mol.custom.utils import convert_to_pyg_graph
+from fs_mol.data.fsmol_task import FSMolTask, PyG_MoleculeDatapoint
+from typing import List, Tuple
+from dpu_utils.utils.richpath import RichPath
+
+
+class PyGMolTask(FSMolTask):
+    name: str
+    samples: List[PyG_MoleculeDatapoint]
+    
+    def get_pos_neg_separated(self) -> Tuple[List[PyG_MoleculeDatapoint], List[PyG_MoleculeDatapoint]]:
+        pos_samples, neg_samples = partition(pred=lambda s: s.y, iterable=self.samples)
+        return list(pos_samples), list(neg_samples)
+    
+    def load_from_file(path: RichPath) -> "PyGMolTask":
+        fsmol_task = FSMolTask.load_from_file(path)
+        
+        moleculeDatapoints = fsmol_task.samples
+        
+        pyg_moleculardatapoints = list(map(convert_to_pyg_graph, moleculeDatapoints))
+        
+        return PyGMolTask(fsmol_task.name, pyg_moleculardatapoints)
