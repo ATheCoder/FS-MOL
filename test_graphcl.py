9aedbf526ecca9e7a1c66282e286ac911fc91cb6
Arash Arbabi <arbabiarash0@gmail.com>
2022-11-26 18:35:37 +0330

    fix: bug when running on cuda

--- test_graphcl.py
+++ test_graphcl.py
@@ -17,7 +17,7 @@ MODEL_LOCATION = './pretraining_feature_extractor_48.pt'
 
 encoderModel = torch.load(MODEL_LOCATION)
 
-validation_dataset = FSMolDataset.from_directory('./datasets/fs-mol', task_list_file="./datasets/fsmol-0.1.json")
+validation_dataset = FSMolDataset.from_directory('./datasets/fs-mol', task_list_file="./datasets/fsmol-0.1.json", num_workers=0)
 
 def validate_model(encoderModel):
     model = PyG_PrototypicalNetwork(encoderModel)
@@ -53,7 +53,7 @@ def validate_model(encoderModel):
             test_model_fn=test_model_fn,
             dataset=validation_dataset,
             train_set_sample_sizes=[16, 32, 64, 128, 256], # This is from parse_command_line in `protonet_train.py`
-            out_dir='outputs/FSMol_Eval_ProtoNet_2022-11-26_14-19-50', # What is save Dir? Seems like this is None
+            # out_dir='outputs/FSMol_Eval_ProtoNet_2022-11-26_14-19-50', # What is save Dir? Seems like this is None
             num_samples=10, # `PrototypicalNetworkTrainerConfig` from `protonet_utils.py`
             task_reader_fn=pyg_task_reader_fn,
             fold=DataFold.TEST, # This is used from `validate_by_finetuning_on_tasks` in `protonet_utils.py` This is validation because during the training loop inside the `train_loop` function FS-MOL calls `validate_by_finetuning_on_tasks`
