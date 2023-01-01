da0c311b0018c979957cf03e7b0db37da6b954aa
Arash Arbabi <arbabiarash0@gmail.com>
2023-01-01 17:58:46 +0000

    feat: add batch_norm and residual connections

--- fs_mol/modules/bidirectional_attention.py
+++ fs_mol/modules/bidirectional_attention.py
@@ -2,7 +2,7 @@ import torch
 from torch import nn
 
 class CrossAttention(nn.Module):
-    def __init__(self, feat1_dim, feat2_dim, dim, qkv_bias = False, attn_drop = 0.2, proj_drop = 0.):
+    def __init__(self, feat1_dim, feat2_dim, dim, qkv_bias = False, attn_drop = 0., proj_drop = 0.):
         super().__init__()
         self.scale = dim ** -0.5
         
@@ -29,14 +29,51 @@ class CrossAttention(nn.Module):
         result = (attn @ v)
         
         return result
+
+
+class CrossAttentionBlock(nn.Module):
+    def __init__(self, feat1_dim, feat2_dim, dim, qkv_bias = False, attn_drop = 0., proj_drop = 0.) -> None:
+        super().__init__()
         
+        self.cross_attention = CrossAttention(dim, dim, dim, qkv_bias, attn_drop, proj_drop)
+        
+        # These two are used to make the dim the same for both feat1 and feat2
+        self.feat1_FFN = nn.Sequential(nn.Linear(feat1_dim, dim), nn.ReLU())
+        self.feat2_FFN = nn.Sequential(nn.Linear(feat2_dim, dim), nn.ReLU())
+        
+        # Layer Norms for both features
+        self.feat1_normalizer = nn.LayerNorm(dim)
+        self.feat2_normalizer = nn.LayerNorm(dim)
+        
+        self.fused_embedding_normalizer = nn.LayerNorm(dim)
+        
+        # Fused embedding FFN
+        self.fused_norm_FFN = nn.Sequential(nn.Linear(dim, dim), nn.ReLU())
+    
+    def forward(self, feat1, feat2):
+        feat1 = self.feat1_FFN(feat1)
+        feat2 = self.feat2_FFN(feat2)
+        
+        feat1_norm = self.feat1_normalizer(feat1)
+        feat2_norm = self.feat2_normalizer(feat2)
+        
+        fused_embedding = self.cross_attention(feat1_norm, feat2_norm)
+        
+        # Addition Step
+        fused_embedding = feat1 + fused_embedding
+        
+        fused_embedding_norm = self.fused_embedding_normalizer(fused_embedding)
+        
+        fused_embedding_norm = self.fused_norm_FFN(fused_embedding_norm)
+        
+        return fused_embedding + fused_embedding_norm
 
 class BidirectionalAttention(nn.Module):
     def __init__(self, graph_embedding_dim, mol_descs_dim, dim, emb_dim):
         super().__init__()
-        self.graph_to_desc_attn = CrossAttention(graph_embedding_dim, mol_descs_dim, dim)
+        self.graph_to_desc_attn = CrossAttentionBlock(graph_embedding_dim, mol_descs_dim, dim)
         
-        self.desc_to_graph_attn = CrossAttention(mol_descs_dim, graph_embedding_dim, dim)
+        self.desc_to_graph_attn = CrossAttentionBlock(mol_descs_dim, graph_embedding_dim, dim)
         
         self.aggr_proj = nn.Linear(2 * dim, emb_dim)
         
